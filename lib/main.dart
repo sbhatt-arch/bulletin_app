0ed4e41b0af62e1e66b82fb38121858fe01c87a2
AkashChatterjee1999 <akashchatterjee1000@gmail.com>
2020-06-26 10:23:46 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bulletin App',
+      home: home_page(),
+    );
+  }
+}
+class home_page extends StatefulWidget {
+  @override
+  _home_pageState createState() => _home_pageState();
+}
+
+class _home_pageState extends State<home_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO: Complete the first landing page functionality here
+    );
+  }
+}
